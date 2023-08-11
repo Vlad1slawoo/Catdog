d826b010b77b1ec5f2a834f8b1e8a005a9370aad
Vlad1slawoo <115892397+Vlad1slawoo@users.noreply.github.com>
2023-08-11 13:41:31 +0300

    Update main.py
    котопес

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption='Загруженая фотография', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Определяем...")
     label = predict(image)
-    st.write(f'The uploaded image most likely belongs to a {label}.')
+    st.write(f'Скорее всего на фотографии {label}.')
 
